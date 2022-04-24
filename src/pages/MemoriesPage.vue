c64e513a0830873a86867033c85a74fc7c1cb75a
Thomas Pietschmann <thomas@pietschi.com>
2022-04-24 18:19:48 +0200

    change theme

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>All Memories</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+<ion-list>
+  <ion-item>Fuck</ion-item>
+  <ion-item>Dreck</ion-item>
+  <ion-item>Bums</ion-item>
+  <ion-item>Fuck</ion-item>
+  <ion-item>Dreck</ion-item>
+  <ion-item>Bums</ion-item>
+  <ion-item>Fuck</ion-item>
+  <ion-item>Dreck</ion-item>
+  <ion-item>Bums</ion-item>
+</ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem
+} from "@ionic/vue";
+
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonContent,
+    IonToolbar,
+    IonList,
+    IonItem
+  },
+};
+</script>
