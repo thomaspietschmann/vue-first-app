0346bf1317bf2d7ef22a5689fff181f83df96bb2
Thomas Pietschmann <thomas.pietschmann@gmail.com>
2022-04-24 18:56:31 +0200

    add BaseLayout

---
+++ src/components/base/BaseLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>{{ title }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <slot></slot>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+
+export default {
+  props: ["title"],
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+  },
+};
+</script>
