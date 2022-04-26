f2b599c5d088eacb96266a886f674c6ecd2b047b
Thomas Pietschmann <thomas.pietschmann@gmail.com>
2022-04-26 16:02:59 +0200

    fix Memory Details refactoring

---
+++ src/components/memories/MemoryOverview.vue
@@ -0,0 +1,16 @@
+<template>
+  <ion-img :src="image" :alt="title"></ion-img>
+  <h2>{{ title }}</h2>
+  <p>{{ text }}</p>
+</template>
+
+<script>
+import { IonImg } from "@ionic/vue";
+
+export default {
+  props: ["title", "image", "text"],
+  components: {
+    IonImg,
+  },
+};
+</script>
--- src/pages/MemoryDetailsPage.vue
+++ src/pages/MemoryDetailsPage.vue
@@ -1,12 +1,24 @@
 <template>
   <base-layout :title="memory ? memory.title : 'Loading'" page-default-back-link="/memories">
     <h2 v-if="!memory">Could not find a memory for the given ID</h2>
-    <h2 v-else>{{memory.title}}</h2>
+    <h2 v-else>
+      <memory-overview
+        :title="memory.title"
+        :text="memory.text"
+        :image="memory.image">
+        </memory-overview>
+    </h2>
   </base-layout>
 </template>
 
 <script>
+
+import MemoryOverview from "../components/memories/MemoryOverview.vue";
+
 export default {
+  components: {
+    MemoryOverview,
+  },
   mounted() {
     console.log(this.memoryId);
     return this.$store.getters.memory(this.memoryId);
