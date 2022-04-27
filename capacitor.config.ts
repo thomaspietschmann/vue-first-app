21320fe08b1043acf84065eb881660386ad93fb2
Thomas Pietschmann <thomas.pietschmann@gmail.com>
2022-04-27 11:06:28 +0200

    fix android build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.gruseljs.de',
   appName: 'vue-first-app',
-  webDir: 'public',
+  webDir: 'dist',
   bundledWebRuntime: false
 };
 
